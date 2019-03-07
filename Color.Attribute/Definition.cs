e40318dd0c9aac7246ff3e7bf69015e839925e6c
Benio <git@benio.me>
2019-03-07 23:46:00 +0100

    Make ClassificationTypeDefinitions readonly.

--- Color.Attribute/Definition.cs
+++ Color.Attribute/Definition.cs
@@ -13,74 +13,74 @@ namespace Color.Attribute
 
 		[Export(typeof(ClassificationTypeDefinition))]
 		[Name("Attribute")]
-		private static ClassificationTypeDefinition
+		private static readonly ClassificationTypeDefinition
 		Definition_Attribute;
 
 		[Export(typeof(ClassificationTypeDefinition))]
 		[Name("Attribute.Squares")]
-		private static ClassificationTypeDefinition
+		private static readonly ClassificationTypeDefinition
 		Definition_Attribute_Squares;
 
 		[Export(typeof(ClassificationTypeDefinition))]
 		[Name("Attribute.Punct")]
-		private static ClassificationTypeDefinition
+		private static readonly ClassificationTypeDefinition
 		Definition_Attribute_Punct;
 
 		[Export(typeof(ClassificationTypeDefinition))]
 		[Name("Attribute.carries_dependency")]
-		private static ClassificationTypeDefinition
+		private static readonly ClassificationTypeDefinition
 		Definition_Attribute_carries_dependency;
 
 		[Export(typeof(ClassificationTypeDefinition))]
 		[Name("Attribute.fallthrough")]
-		private static ClassificationTypeDefinition
+		private static readonly ClassificationTypeDefinition
 		Definition_Attribute_fallthrough;
 
 		[Export(typeof(ClassificationTypeDefinition))]
 		[Name("Attribute.likely")]
-		private static ClassificationTypeDefinition
+		private static readonly ClassificationTypeDefinition
 		Definition_Attribute_likely;
 
 		[Export(typeof(ClassificationTypeDefinition))]
 		[Name("Attribute.maybe_unused")]
-		private static ClassificationTypeDefinition
+		private static readonly ClassificationTypeDefinition
 		Definition_Attribute_maybe_unused;
 
 		[Export(typeof(ClassificationTypeDefinition))]
 		[Name("Attribute.no_unique_address")]
-		private static ClassificationTypeDefinition
+		private static readonly ClassificationTypeDefinition
 		Definition_Attribute_no_unique_address;
 
 		[Export(typeof(ClassificationTypeDefinition))]
 		[Name("Attribute.nodiscard")]
-		private static ClassificationTypeDefinition
+		private static readonly ClassificationTypeDefinition
 		Definition_Attribute_nodiscard;
 
 		[Export(typeof(ClassificationTypeDefinition))]
 		[Name("Attribute.noreturn")]
-		private static ClassificationTypeDefinition
+		private static readonly ClassificationTypeDefinition
 		Definition_Attribute_noreturn;
 
 		[Export(typeof(ClassificationTypeDefinition))]
 		[Name("Attribute.optimize_for_synchronized")]
-		private static ClassificationTypeDefinition
+		private static readonly ClassificationTypeDefinition
 		Definition_Attribute_optimize_for_synchronized;
 
 		[Export(typeof(ClassificationTypeDefinition))]
 		[Name("Attribute.unlikely")]
-		private static ClassificationTypeDefinition
+		private static readonly ClassificationTypeDefinition
 		Definition_Attribute_unlikely;
 
 		#region Attribute.Deprecated
 
 		[Export(typeof(ClassificationTypeDefinition))]
 		[Name("Attribute.Deprecated.Mark")]
-		private static ClassificationTypeDefinition
+		private static readonly ClassificationTypeDefinition
 		Definition_Attribute_Deprecated_Mark;
 
 		[Export(typeof(ClassificationTypeDefinition))]
 		[Name("Attribute.Deprecated.Reason")]
-		private static ClassificationTypeDefinition
+		private static readonly ClassificationTypeDefinition
 		Definition_Attribute_Deprecated_Reason;
 
 		#endregion
@@ -88,44 +88,44 @@ namespace Color.Attribute
 
 		[Export(typeof(ClassificationTypeDefinition))]
 		[Name("Attribute.Contract.Expression")]
-		private static ClassificationTypeDefinition
+		private static readonly ClassificationTypeDefinition
 		Definition_Attribute_Contract_Expression;
 
 		[Export(typeof(ClassificationTypeDefinition))]
 		[Name("Attribute.Contract.Assert.Mark")]
-		private static ClassificationTypeDefinition
+		private static readonly ClassificationTypeDefinition
 		Definition_Attribute_Contract_Assert_Mark;
 
 		[Export(typeof(ClassificationTypeDefinition))]
 		[Name("Attribute.Contract.Ensures.Mark")]
-		private static ClassificationTypeDefinition
+		private static readonly ClassificationTypeDefinition
 		Definition_Attribute_Contract_Ensures_Mark;
 
 		[Export(typeof(ClassificationTypeDefinition))]
 		[Name("Attribute.Contract.Ensures.Identifier")]
-		private static ClassificationTypeDefinition
+		private static readonly ClassificationTypeDefinition
 		Definition_Attribute_Contract_Ensures_Identifier;
 
 		[Export(typeof(ClassificationTypeDefinition))]
 		[Name("Attribute.Contract.Expects.Mark")]
-		private static ClassificationTypeDefinition
+		private static readonly ClassificationTypeDefinition
 		Definition_Attribute_Contract_Expects_Mark;
 
 		#region Attribute.Contract.Level
 
 		[Export(typeof(ClassificationTypeDefinition))]
 		[Name("Attribute.Contract.Level.Audit")]
-		private static ClassificationTypeDefinition
+		private static readonly ClassificationTypeDefinition
 		Definition_Attribute_Contract_Level_Audit;
 
 		[Export(typeof(ClassificationTypeDefinition))]
 		[Name("Attribute.Contract.Level.Axiom")]
-		private static ClassificationTypeDefinition
+		private static readonly ClassificationTypeDefinition
 		Definition_Attribute_Contract_Level_Axiom;
 
 		[Export(typeof(ClassificationTypeDefinition))]
 		[Name("Attribute.Contract.Level.Default")]
-		private static ClassificationTypeDefinition
+		private static readonly ClassificationTypeDefinition
 		Definition_Attribute_Contract_Level_Default;
 
 		#endregion
